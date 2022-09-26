93364183e1690580eca9732dd5b0f82663fa6f3e
Yash Verma <vyash5075@gmail.com>
2022-09-26 18:44:58 +0530

    Update ChatRoom.jsx

--- FE/src/components/ChatRoom.jsx
+++ FE/src/components/ChatRoom.jsx
@@ -15,7 +15,7 @@ const ChatRoom = () => {
     const [ socket, setSocket ] = useState()
 
     useEffect(() => {
-        const socket = io("http://localhost:9000")
+        const socket = io("http://localhost:8000")
         setSocket(socket)
 
         socket.on("connect", () => {
